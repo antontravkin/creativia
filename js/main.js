c9af4f6e8f7c2122a547fd90379987060eaaef21
Антон Травкин <antontravkin@MacBook-Pro-Anton.local>
2019-02-13 22:32:48 +0300

    skills

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    
+
     $("#menu").on("click","a", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
@@ -8,5 +10,33 @@ $(document).ready(function() {
 	$('.about__info').on('click', '.info', function() {
         $(this).toggleClass('text_red').siblings('.info__text').slideToggle(400);
         $(this).find('i').toggleClass('fa-minus-circle').toggleClass('fa-plus-circle');
-	});
+    });
+
+ 
+	
+    var a = true;   
+    function x(){
+        var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-36251023-1']);
+            _gaq.push(['_setDomainName', 'jqueryscript.net']);
+            _gaq.push(['_trackPageview']);
+        $('.circlechart').circlechart(); 
+        (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+        a = false;
+    } 
+    
+    var $win = $(window);
+    var $marker = $('.skills');
+$win.scroll(function() {
+    if($win.scrollTop() + $win.height() >= $marker.offset().top) {
+        if(a)
+            x();
+    }
+});
+
+    
+    
 });
